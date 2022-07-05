c1123eda70026b69679b2ae426a0bf831283f218
PGCranford <PGCranford@gmail.com>
2022-07-05 16:07:15 -0400

    code switch

--- server.js
+++ server.js
@@ -50,17 +50,22 @@ function findById(id, animalsArray) {
 
 
 app.get('/api/animals', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
     }
+    res.json(results);
 });
 
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     res.json(result);
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
 });
 
 app.post('/api/animals', (req, res) => {
